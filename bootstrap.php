d3c6f86d8aacf9be27997928fecfb48f723c7180
Sean McCafferty <smccafferty@voceconnect.com>
2016-02-22 16:53:24 -0500

    Abstracting function and init call

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,6 @@
 if ( defined( 'ABSPATH' ) && function_exists('add_action') ) {
 	if ( ! has_action( 'init', array( 'Voce_Error_Logging', 'create_post_type' ) ) ) {
 		Voce_Error_Logging::init();
+		require __DIR__ . '/functions.php';
 	}
 }
--- voce-error-logging.php
+++ voce-error-logging.php
@@ -202,16 +202,3 @@ class Voce_Error_Logging {
 	}
 
 }
-
-Voce_Error_Logging::init();
-
-// create convenience function for logging
-if ( !function_exists( 'voce_error_log' ) ) {
-
-	function voce_error_log( $title, $error, $tags = array( ) ) {
-		return Voce_Error_Logging::error_log( $title, $error, $tags );
-	}
-
-}
-
-
